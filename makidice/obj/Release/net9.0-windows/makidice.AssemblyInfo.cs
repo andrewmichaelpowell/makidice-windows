edbfd4d1d982b9a51c188a378e3f35b1edca45ff
Andrew Powell <andrewmichaelpowell@icloud.com>
2026-06-20 22:42:35 +0200

    Project update

--- makidice/obj/Release/net9.0-windows/makidice.AssemblyInfo.cs
+++ makidice/obj/Release/net9.0-windows/makidice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("makidice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("3.4.1")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2381bca1142a675ed3133a62609238a601ef8c20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8327f73d907413574f92b38395f8864efee098e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("makidice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Maki Dice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("3.4.1")]
