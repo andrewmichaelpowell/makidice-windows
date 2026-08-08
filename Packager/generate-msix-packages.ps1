bce51a2887b466fba78e02a87e634be9a9a133b5
Andrew Powell <andrewmichaelpowell@icloud.com>
2026-08-08 17:59:45 +0200

    Update packager

--- Packager/generate-msix-packages.ps1
+++ Packager/generate-msix-packages.ps1
@@ -2,14 +2,14 @@
 # github.com/andrewmichaelpowell
 
 $makepri = (Get-ChildItem -Path C:\ -Filter MakePri.exe -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1).FullName
-& $makepri createconfig /cf "C:\Packager\priconfig.xml" /dq en-US /o
+& $makepri createconfig /cf "C:\Package\priconfig.xml" /dq en-US /o
 
 $makeappx = (Get-ChildItem -Path C:\ -Filter MakeAppx.exe -Recurse -ErrorAction SilentlyContinue | Select-Object -First 1).FullName
 
-& $makepri new /pr "C:\Packager\win-x64\publish" /cf "C:\Packager\priconfig.xml" /mn "C:\Packager\win-x64\publish\AppxManifest.xml" /of "C:\Packager\win-x64\publish\resources.pri" /o
-& $makeappx pack /d "C:\Packager\win-x64\publish" /p "C:\Packager\msix\MakiDice_x64.msix" /o
+& $makepri new /pr "C:\Package\win-x64\publish" /cf "C:\Package\priconfig.xml" /mn "C:\Package\win-x64\publish\AppxManifest.xml" /of "C:\Package\win-x64\publish\resources.pri" /o
+& $makeappx pack /d "C:\Package\win-x64\publish" /p "C:\Package\msix\MakiDice_x64.msix" /o
 
-& $makepri new /pr "C:\Packager\win-arm64\publish" /cf "C:\Packager\priconfig.xml" /mn "C:\Packager\win-arm64\publish\AppxManifest.xml" /of "C:\Packager\win-arm64\publish\resources.pri" /o
-& $makeappx pack /d "C:\Packager\win-arm64\publish" /p "C:\Packager\msix\MakiDice_arm64.msix" /o
+& $makepri new /pr "C:\Package\win-arm64\publish" /cf "C:\Package\priconfig.xml" /mn "C:\Package\win-arm64\publish\AppxManifest.xml" /of "C:\Package\win-arm64\publish\resources.pri" /o
+& $makeappx pack /d "C:\Package\win-arm64\publish" /p "C:\Package\msix\MakiDice_arm64.msix" /o
 
-& $makeappx bundle /d "C:\Packager\msix" /p "C:\Packager\MakiDice.msixbundle" /o
+& $makeappx bundle /d "C:\Package\msix" /p "C:\Package\MakiDice.msixbundle" /o
