5a87b3c50ee7710d249652d333efdb4908b89f10
Andrew Powell <andrewmichaelpowell@icloud.com>
2026-08-08 17:55:52 +0200

    Packager update

---
+++ build.sh
@@ -0,0 +1,4 @@
+rm -fr bin
+rm -fr obj
+dotnet publish -c Release -r win-x64 --self-contained -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfContained=true -p:EnableWindowsTargeting=true
+dotnet publish -c Release -r win-arm64 --self-contained -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfContained=true -p:EnableWindowsTargeting=true
