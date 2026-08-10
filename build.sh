04fd83a56d59c7d9b890b202f60347e854ddd7f4
Andrew Powell <andrewmichaelpowell@icloud.com>
2026-08-10 06:41:42 +0200

    Update packages

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+# Maki Dice (Windows)
+# github.com/andrewmichaelpowell
+
 rm -fr bin
 rm -fr obj
 dotnet publish -c Release -r win-x64 --self-contained -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfContained=true -p:EnableWindowsTargeting=true
