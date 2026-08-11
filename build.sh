741ea09d153d566dca452a5c5e723f45d4c88420
Andrew Powell <andrewmichaelpowell@icloud.com>
2026-08-11 21:47:36 +0200

    Update build

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@
 
 rm -fr bin
 rm -fr obj
-dotnet publish -c Release -r win-x64 --self-contained -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfContained=true -p:EnableWindowsTargeting=true
 dotnet publish -c Release -r win-arm64 --self-contained -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfContained=true -p:EnableWindowsTargeting=true
+dotnet publish -c Release -r win-x64 --self-contained -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfContained=true -p:EnableWindowsTargeting=true
+zip -r ./bin/makidice-windows-arm64.zip -j ./bin/Release/net9.0-windows/win-arm64/publish/*
+zip -r ./bin/makidice-windows-x64.zip -j ./bin/Release/net9.0-windows/win-x64/publish/*
