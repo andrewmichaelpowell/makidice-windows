e8f5d9f7edffe3a55701cffba18a8672e9e66005
Andrew Powell <andrewmichaelpowell@icloud.com>
2026-08-11 22:23:43 +0200

    Update build

--- build.ps1
+++ build.ps1
@@ -1,8 +1,8 @@
 # Maki Dice (Windows)
 # github.com/andrewmichaelpowell
 
-if (Test-Path .\bin) { Remove-Item -Recurse -Force .\bin }
-if (Test-Path .\obj) { Remove-Item -Recurse -Force .\obj }
+if (Test-Path ./bin) { Remove-Item -Recurse -Force ./bin }
+if (Test-Path ./obj) { Remove-Item -Recurse -Force ./obj }
 
 dotnet publish -c Release -r win-arm64 --self-contained -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfContained=true -p:EnableWindowsTargeting=true
 dotnet publish -c Release -r win-x64 --self-contained -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfContained=true -p:EnableWindowsTargeting=true
